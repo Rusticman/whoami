9f8ba1be144d10f539a5651576919e11e39a5d19
rusticman <russellwheatley85@gmail.com>
2016-07-27 14:00:33 +0100

    second draft

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 var express = require('express');
-var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var app = express();
+var results = {ipaddress:null,language:null,software:null};
+
+
+app.set('port',process.env.PORT || 5000);
+
+app.use('/', function(req, res, next) {
+  results.ipaddress = req.headers['x-forwarded-for'] ||
+       req.connection.remoteAddress ||
+       req.socket.remoteAddress ||
+       req.connection.socket.remoteAddress;
+  results.language = req.headers["accept-language"].split(',')[0];
+  var regExp = /\(([^)]+)\)/;
+  results.software = req.headers["user-agent"].match(regExp)[1];
+
+res.json(results);
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+
+
+app.listen(app.get('port'),function(){
+  console.log('listening on:' + app.get('port'));
 });
